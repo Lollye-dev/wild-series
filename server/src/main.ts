0fc1582e3642127983667c10d9dbd54690226794
Lollye-dev <lorenvisset@gmail.com>
2025-12-15 14:31:59 +0100

    Affichage du message Welcome to Wild Series !

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,12 +7,19 @@ import "dotenv/config";
 import "../database/checkConnection";
 
 // Import the Express application from ./app
+import type { RequestHandler } from "express";
 import app from "./app";
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
 // Start the server and listen on the specified port
+app.get("/", sayWelcome);
+
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
